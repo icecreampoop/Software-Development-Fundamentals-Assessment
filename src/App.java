6245096f06a9d680bf483dcf58b5175f27bbab78
icecreampoop <pchiku@hotmail.sg>
2024-03-01 12:16:19 +0800

    task 1 done

--- src/App.java
+++ src/App.java
@@ -1,14 +1,19 @@
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    static HashMap<Integer, List<String>> pokemonStackStore = new HashMap<>();
     public static void main(String[] args) throws Exception {
         String welcome = """
                 Welcome to Pokemon Gaole Legend 4 Rush 2
 
-                (1) View unique list of Pokemon in the selected stack
-                (2) Find next 5 stars Pokemon occurrence
-                (3) Create new Pokemon stack and save (append) to csv file
-                (4) Print distinct Pokemon and cards count
+                (1) View the list of Pokemon in the selected stack
+                (2) View unique list of Pokemon in the selected stackv
+                (3) Find next 5 stars Pokemon occurrence
+                (4) Create new Pokemon stack and save (append) to csv file
                 (q) to exit the program
                 Enter your selection >""";
         boolean userQuit = false;
@@ -17,14 +22,30 @@ public class App {
 
         System.out.print(welcome);
 
+        //storing csv data
+        FileService fileService = new FileService();
+        int count = 1;
+         for (String lines : fileService.csvReader(args[0])) {
+            pokemonStackStore.put(count, Arrays.asList(lines.split(",")));
+            count++;
+         }
+
         while (!userQuit) {
-            userInput = scan.nextLine();
+            userInput = scan.nextLine().toLowerCase().trim();
 
             if (userInput.equals("q")) {
                 System.out.println();
                 System.out.println("Thank you for using the program...");
                 System.out.println("Hope to see you soon...");
                 userQuit = true;
+            }
+
+            if (userInput.equals("4")) {
+                System.out.println("Create a new Pokemon stack and save to a new file >");
+                String fileData = scan.nextLine();
+                System.out.println("Enter filename to save (e.g. path/filename.csv) >");
+                String filePath = scan.nextLine();
+                fileService.writeAsCSV(fileData, Paths.get(filePath));
             }
         }
 
--- src/FileService.java
+++ src/FileService.java
@@ -1,3 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class FileService {
-    
+    private Path path;
+    private String lines = "";
+    private ArrayList<String> temp = new ArrayList<>();
+
+    public ArrayList<String> csvReader(String csvPath) {
+        temp.clear();
+        path = Paths.get(csvPath);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path.toFile()))) {
+            while ((lines = bufferedReader.readLine()) != null) {
+                temp.add(lines);
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+        return temp;
+    }
+
+    public void writeAsCSV(String fileData, Path filePath) {
+        //if it doesnt exist
+        if (!Files.exists(filePath)) {
+            try {
+                Files.createFile(filePath);
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
+        //appending to file
+        ArrayList<String> existingData = csvReader(filePath.toString());
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath.toFile()))) {
+            for (String x : existingData) {
+                bufferedWriter.write(x);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write(fileData);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
 }
